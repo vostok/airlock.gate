e100fb09de88c118e91401380ae2093a3293638b
andrew <Andrew.Kostousov@gmail.com>
2017-10-02 14:47:35 +0500

    transfer code to vostok/airlock.consumer

--- AirlockConsumer/AirlockConsumer/IMessageProcessor.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace AirlockConsumer
-{
-    public interface IMessageProcessor<T>
-    {
-        void Process(IEnumerable<ConsumerEvent<T>> events);
-    }
-}
--- AirlockConsumer/AirlockLogConsumer/AirlockLogEventSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AirlockLogConsumer
-{
-    public class AirlockLogEventSettings
-    {
-        public int BatchSize { get; set; } = 1000;
-        public string[] ElasticUriList { get; set; }
-    }
-}
