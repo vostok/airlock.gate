6e73838a205c500620d2a163eae187f58b6f90a7
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2017-10-04 18:32:15 +0500

    add test of C# deserialization

--- src/test/java/SerializationTest.java
+++ src/test/java/SerializationTest.java
@@ -1,7 +1,10 @@
+import org.junit.Assert;
 import org.junit.Test;
 import org.unitils.reflectionassert.ReflectionAssert;
 import ru.kontur.airlock.dto.AirlockMessage;
 import ru.kontur.airlock.dto.BinarySerializable;
+
+import java.util.Base64;
 
 public class SerializationTest {
     @Test
@@ -11,5 +14,12 @@ public class SerializationTest {
         byte[] bytes = message.toByteArray();
         BinarySerializable obj2 = BinarySerializable.fromByteArray(bytes, AirlockMessage.class);
         ReflectionAssert.assertReflectionEquals(message, obj2);
+    }
+
+    @Test
+    public void CSharpGeneratedBinaryDeserialization() throws Exception {
+        byte[] bytes = Base64.getDecoder().decode("AQAIAAAADwAAAGlsb2t0aW9ub3YtdGVzdAAAAAAPAAAAaWxva3Rpb25vdi10ZXN0AAAAAA8AAABpbG9rdGlvbm92LXRlc3QAAAAADwAAAGlsb2t0aW9ub3YtdGVzdAAAAAAPAAAAaWxva3Rpb25vdi10ZXN0AQAAAO1AkudeAQAAKAAAACQAAABhMjkwNWI1YS05MTYzLTQ0NDktYmYyNS1hM2EyOGZjNTIwM2YPAAAAaWxva3Rpb25vdi10ZXN0AQAAAOdBkudeAQAAKAAAACQAAABlNjE1M2U1MS00ZTI1LTQwNjMtYmE1NS0yMTU4ZmZmNGFhYmUPAAAAaWxva3Rpb25vdi10ZXN0AQAAAOJCkudeAQAAKAAAACQAAAA0NTAxNGY2YS01ZmUzLTQ0YWItODM3Yy1iYzU0OTA5ZWExNWMPAAAAaWxva3Rpb25vdi10ZXN0AQAAANxDkudeAQAAKAAAACQAAABlYzQ2ODNiZS0yYWNmLTQ1YjctYjg3YS01NDRjNDg0NTJjOGM=");
+        AirlockMessage csharpMessage = BinarySerializable.fromByteArray(bytes, AirlockMessage.class);
+        Assert.assertEquals(8, csharpMessage.eventGroups.size());
     }
 }
