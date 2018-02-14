02fdb67804d496362c84b013b825ec1dd19cc5bf
lapin <lapin@kontur.ru>
2018-02-14 19:51:47 +0400

    text autoformat

--- src/test/java/ThrottlingTest.java
+++ src/test/java/ThrottlingTest.java
@@ -1,22 +1,23 @@
+import static org.mockito.Mockito.mock;
+
 import com.google.common.base.Stopwatch;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 import org.apache.http.HttpStatus;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import ru.kontur.airlock.EventSender;
-import ru.kontur.airlock.dto.EventGroup;
-import ru.kontur.airlock.dto.EventRecord;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
-import java.util.*;
-import java.util.concurrent.TimeUnit;
-
-import static org.mockito.Mockito.*;
-import static org.junit.runners.Parameterized.*;
 
 @RunWith(Parameterized.class)
 public class ThrottlingTest extends HttpServerTestBase {
+
     @Parameter(0)
     public int eventCount;
     @Parameter(1)
@@ -24,7 +25,7 @@ public class ThrottlingTest extends HttpServerTestBase {
 
     @Parameters
     public static Collection<Object[]> data() {
-        Object[][] data = new Object[][] { { 150 , 1 }, { 5, 0 }, { 250, 2 } };
+        Object[][] data = new Object[][]{{150, 1}, {5, 0}, {250, 2}};
         return Arrays.asList(data);
     }
 
@@ -37,12 +38,13 @@ public class ThrottlingTest extends HttpServerTestBase {
     @Test
     public void throttlingTest() throws Exception {
         final Stopwatch stopwatch = Stopwatch.createStarted();
-        for (int i=0; i<eventCount; i++) {
-            testSend(AirlockMessageGenerator.generateAirlockMessage("project.env.a"), HttpStatus.SC_OK);
+        for (int i = 0; i < eventCount; i++) {
+            testSend(AirlockMessageGenerator.generateAirlockMessage("project.env.a"),
+                    HttpStatus.SC_OK);
         }
         stopwatch.stop();
         final long elapsed = stopwatch.elapsed(TimeUnit.SECONDS);
-        Assert.assertTrue( "elapsed too small: " + elapsed,elapsed == secondsElapsed);
+        Assert.assertTrue("elapsed too small: " + elapsed, elapsed == secondsElapsed);
     }
 
 }
